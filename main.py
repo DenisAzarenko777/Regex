c4975e61de441910162dfd3a1400d2ad6f21a4cf
Denis Azarenko <den.azarenko.98@gmail.com>
2020-11-04 14:46:57 +0800

    The fird commit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import re
 with open("phonebook_raw.csv", encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-print(contacts_list)
 # TODO 1: выполните пункты 1-3 ДЗ
 # ваш код
 some_list = []
@@ -17,7 +16,6 @@ for i in range(0,len(contacts_list),1):
             some_list.append(contacts_list[i][j])
         else:
             if j == 0:
-                print(re.compile("[а-яёА-ЯЁ]+").findall(contacts_list[i][j]))
                 patt = re.compile("[а-яёА-ЯЁ]+").findall(contacts_list[i][j])
                 if len(patt) >= 1:
                     some_list.append(patt[0])
@@ -32,7 +30,6 @@ for i in range(0,len(contacts_list),1):
                 if len(patt) >= 2:
                     some_list.append(patt[1])
 
-            print(len(patt))
             if j == 2:
                 if len(patt) >= 3:
                     some_list.append(patt[2])
@@ -50,6 +47,9 @@ for i in range(0,len(contacts_list),1):
                 some_list.append(contacts_list[i][j])
     some_list2.append(some_list)
     some_list = []
+print(some_list2)
+
+
 
 #TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
